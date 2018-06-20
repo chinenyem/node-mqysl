f81e87f602c84e81e20260afc81f611c39dc771f
chinenyem.nwadiugwu <chinenyem.nwadiugwu@centurum.com>
2018-06-20 10:17:48 -0400

    Setup knex

---
+++ migrations/20180620095523_create_user_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function (knex) {
+  return knex.schema.createTable('user', function (t) {
+    t.increments('id').primary()
+    t.string('username').notNullable()
+    t.string('password').notNullable()
+    t.timestamp('created_at').defaultTo(knex.fn.now());
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('user')
+}
