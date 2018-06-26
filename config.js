e49a7ee94a11d5dc0a1d99087369ccb8f25d0dd7
chinenyem.nwadiugwu <chinenyem.nwadiugwu@centurum.com>
2018-06-26 15:41:35 -0400

    fixed authentication

--- config.js
+++ config.js
@@ -1,4 +1,19 @@
 // config.js
+
+const cry = require('crypto');
+
+
+const d = new Date();
+const month = d.getMonth();
+const day = d.getDay();
+const year = d.getFullYear();
+const currentDate = new Date(day, month, year);
+
+const todaySecret = cry.randomBytes(48, function(err, buffer) { 
+  var token = buffer.toString('hex');
+  return token 
+});
+
 module.exports = {
-  'secret': 'supersecret'
+  'secret': todaySecret()
 }
