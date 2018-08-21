a9916597c6fc2fc30748073237dc0516a1d920f7
Chinenyem Nwadiugwu <chnw1947@gmail.com>
2018-08-21 16:56:21 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   client: 'mysql',
   connection: {
     user: 'root',
-    password: 'panda87!',
-    database: 'tutorial_node_database'
+    password: '',
+    database: ''
   }
 }
